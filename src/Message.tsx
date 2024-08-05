d284e212543db521fbb611536570b458131d1c1c
Begüm Başovalı <begumbasovali@posta.mu.edu.tr>
2024-08-06 00:06:30 +0300

    first step to React

---
+++ src/Message.tsx
@@ -0,0 +1,13 @@
+
+// important!!! PascalCasing
+
+function Message() {
+  // JSX: JavaScript XML
+  const name = 'Begum';
+  if (name)
+    return <h1>Hello, {name}</h1>;
+  return <h1>Hello, world!</h1>
+
+}
+
+export default Message;
